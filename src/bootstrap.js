d3d8104bcbfff2a0041e92caa283787b2534afd0
LorenzoMarnat <lmarnat14@gmail.com>
2021-11-18 10:59:44 +0100

    Drag and drop & download updated file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,10 +6,61 @@ import { raycastOnPoint, setObjectsToRaycast } from './raycast';
 var layers = null;
 const app = new udviz.Templates.AllWidget();
 
+var body = document.body;
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+function handleDrop(e) {
+  setObjectsToRaycast(layers, '3d-tiles-layer-relief');
+  let dt = e.dataTransfer;
+  for (let file of dt.files) readFile(file);
+}
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
+  body.addEventListener(eventName, preventDefaults, false);
+});
+
+body.addEventListener('drop', handleDrop, false);
+
+function updateValues(fileData, fileName) {
+  var jsonData = JSON.parse(fileData);
+  for (let feature of jsonData.features) {
+    if (feature.geometry.type === 'MultiLineString') {
+      for (let point of feature.geometry.coordinates[0]) {
+        var positionOnGround = raycastOnPoint(point);
+        if (positionOnGround !== undefined && positionOnGround.z > point[2])
+          point[2] = positionOnGround.z;
+      }
+    } else if (feature.geometry.type === 'LineString') {
+      for (let point of feature.geometry.coordinates) {
+        var positionOnGround = raycastOnPoint(point);
+        if (positionOnGround !== undefined && positionOnGround.z > point[2])
+          point[2] = positionOnGround[2];
+      }
+    }
+  }
+  var newJsonData = JSON.stringify(jsonData);
+  var bb = new Blob([newJsonData], { type: 'text/plain' });
+  var a = document.createElement('a');
+  a.download = 'new_' + fileName;
+  a.href = window.URL.createObjectURL(bb);
+  a.click();
+}
+
+function readFile(file) {
+  const reader = new FileReader();
+  reader.addEventListener('load', (event) => {
+    var data = event.target.result;
+    updateValues(data, file.name);
+  });
+  reader.readAsText(file);
+}
+
 app.start('../assets/config/config.json').then((config) => {
   app.addBaseMapLayer();
-
-  // app.addElevationLayer();
 
   layers = app.setupAndAdd3DTilesLayers();
 
@@ -24,45 +75,4 @@ app.start('../assets/config/config.json').then((config) => {
   ////// LAYER CHOICE MODULE
   const layerChoice = new udviz.Widgets.LayerChoice(app.layerManager);
   app.addModuleView('layerChoice', layerChoice);
-
-  console.log(layers);
 });
-
-document.addEventListener(
-  'keydown',
-  (event) => {
-    var name = event.key;
-    if (name === 'r') {
-      setObjectsToRaycast(layers, '3d-tiles-layer-relief');
-
-      udviz.Components.SystemUtils.File.loadJSON('../assets/road.geojson').then(
-        (json) => {
-          for (let feature of json.features) {
-            if (feature.geometry.type === 'MultiLineString') {
-              for (let point of feature.geometry.coordinates[0]) {
-                var positionOnGround = raycastOnPoint(point);
-                if (
-                  positionOnGround !== undefined &&
-                  positionOnGround[2] > point[2]
-                ) {
-                  point[2] = positionOnGround[2];
-                }
-              }
-            } else if (feature.geometry.type === 'LineString') {
-              for (let point of feature.geometry.coordinates) {
-                var positionOnGround = raycastOnPoint(point);
-                if (
-                  positionOnGround !== undefined &&
-                  positionOnGround[2] > point[2]
-                ) {
-                  point[2] = positionOnGround[2];
-                }
-              }
-            }
-          }
-        }
-      );
-    }
-  },
-  false
-);
